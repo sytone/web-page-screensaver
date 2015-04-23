4a58676fdea2ba661490b7bbad230846d534a95b
sytone <github@sytone.com>
2015-04-23 14:58:00 -0700

    Missed File.

--- ScreensaverForm.cs
+++ ScreensaverForm.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 using Microsoft.Win32;
+using WebPageScreensaver.Properties;
+using Timer = System.Windows.Forms.Timer;
 
-namespace pl.polidea.lab.Web_Page_Screensaver
+namespace WebPageScreensaver
 {
     public partial class ScreensaverForm : Form
     {
@@ -21,13 +22,125 @@ namespace pl.polidea.lab.Web_Page_Screensaver
             Application.AddMessageFilter(gueh);
 
             InitializeComponent();
+
+            reloadTimer = new Timer();
+
+            webBrowser.ScriptErrorsSuppressed = true;
+
+            ////RegistryKey zoomReg = Registry.LocalMachine.CreateSubKey("Software\\Microsoft\\Internet Explorer\\Zoom");
+            ////Trace.Write("Zoom Setting: ");
+            ////Trace.WriteLine(zoomReg.GetValue("ZoomDisabled"));
+            ////zoomReg.SetValue("ZoomDisabled", 0, RegistryValueKind.DWord);
+            
+            
         }
+
+        private Timer reloadTimer;
+        private readonly List<Dashboard> dashboards = new List<Dashboard>();
+        private Dashboard currentDashboard;
 
         private void ScreensaverForm_Load(object sender, EventArgs e)
         {
-            RegistryKey reg = Registry.CurrentUser.CreateSubKey(Program.KEY);
-            webBrowser.Navigate((string)reg.GetValue("Url", "http://www.polidea.pl"));
-            reg.Close();
+            string dataFile;
+
+            try
+            {
+                RegistryKey reg = Registry.LocalMachine.CreateSubKey(Program.KEY);
+                dataFile = (string)reg.GetValue("DataPath", ".\\dashboarddata.csv");
+                reg.Close();
+            }
+            catch (Exception)
+            {
+                dataFile = ".\\dashboarddata.csv";
+            }
+            if (!String.IsNullOrWhiteSpace(dataFile))
+            {
+                LoadDashboardsFromFile(dataFile);
+                reloadTimer.Interval = 2000; // Inital load after 2000
+                reloadTimer.Tick += (o, args) => 
+                {
+                    reloadTimer.Stop();
+                    Trace.WriteLine("Timer Ticked!");
+                    currentDashboard = dashboards.Next(currentDashboard);
+                    Trace.WriteLine(currentDashboard);
+
+                    if ((currentDashboard.StartHour == 0 && currentDashboard.EndHour == 0) ||
+                        (DateTime.Now.Hour > currentDashboard.StartHour && DateTime.Now.Hour < currentDashboard.EndHour))
+                    {
+                        webBrowser.Navigate(currentDashboard.Url);
+                        while (webBrowser.IsBusy) { }
+                        Trace.Write("Requested Scale: ");
+                        Trace.WriteLine(currentDashboard.Scale);
+                        this.Focus();
+                        webBrowser.Focus();
+                        switch (currentDashboard.Scale)
+                        {
+                            case -2:
+                                SendKeys.Send("^0");
+                                SendKeys.Send("^+");
+                                SendKeys.Send("^+");
+                                break;
+                            case -1:
+                                SendKeys.Send("^0");
+                                SendKeys.Send("^+");
+                                break;
+                            case 0:
+                                SendKeys.Send("^0");
+                                break;
+                            case 1:
+                                SendKeys.Send("^0");
+                                SendKeys.Send("^-");
+                                break;
+                            case 2:
+                                SendKeys.Send("^0");
+                                SendKeys.Send("^-");
+                                SendKeys.Send("^-");
+                                break;
+                        }
+                        //((SHDocVw.WebBrowser)webBrowser.ActiveXInstance).ExecWB(SHDocVw.OLECMDID.OLECMDID_OPTICAL_ZOOM,
+                        //    SHDocVw.OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, currentDashboard.Scale, IntPtr.Zero);
+                    }
+                    Trace.WriteLine(currentDashboard.Period);
+                    reloadTimer.Interval = currentDashboard.Period * 1000;
+                    Trace.WriteLine(reloadTimer.Interval);
+                    Trace.Write("Timer reset: ");
+                    Trace.WriteLine(reloadTimer.Interval);
+                    if (currentDashboard == dashboards.Last())
+                    {
+                        LoadDashboardsFromFile(dataFile);
+                    }
+                    reloadTimer.Start();
+                };
+                reloadTimer.Start();
+            }
+            else
+            {
+                webBrowser.Navigate("about:blank");
+                Thread.Sleep(10000);
+            }
+            //webBrowser.Navigate((string)reg.GetValue("UncPath", ""));
+            
+        }
+
+
+        private void LoadDashboardsFromFile(string dataFile)
+        {
+            string line;
+            dashboards.Clear();
+            FileStream aFile = new FileStream(dataFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sr = new StreamReader(aFile);
+
+            // read data in line by line
+            while ((line = sr.ReadLine()) != null)
+            {
+                Trace.WriteLine(line);
+                if (line.StartsWith("http") || line.StartsWith("file"))
+                {
+                    dashboards.Add(new Dashboard(line.Split(',')));
+                    Trace.WriteLine(dashboards.Last());
+                }
+            }
+            sr.Close();
         }
 
         private void CloseAfter1Second()
@@ -36,31 +149,5 @@ namespace pl.polidea.lab.Web_Page_Screensaver
                 Close();
         }
 
-    }
-
-    public class GlobalUserEventHandler : IMessageFilter
-    {
-        public delegate void UserEvent();
-        
-        private const int WM_MOUSEMOVE = 0x0200;
-        private const int WM_MBUTTONDBLCLK = 0x209;
-        private const int WM_KEYDOWN = 0x100;
-        private const int WM_KEYUP = 0x101;
-
-        public event UserEvent Event;
-
-        public bool PreFilterMessage(ref Message m)
-        {
-            if ( (m.Msg >= WM_MOUSEMOVE && m.Msg <= WM_MBUTTONDBLCLK) 
-                || m.Msg == WM_KEYDOWN || m.Msg == WM_KEYUP)
-            {
-                if (Event != null)
-                {
-                    Event();
-                }
-            }
-            // Always allow message to continue to the next filter control
-            return false;
-        }
     }
 }
